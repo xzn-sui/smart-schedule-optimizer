cba3d2af0f08574619927472133a605357b89f22
xzn-sui <eric661188@gmail.com>
2026-03-15 03:16:09 -0400

    add s3 url to cors allowed origins

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.include_router(schedules.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://smart-schedule-frontend.s3-website.us-east-2.amazonaws.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
